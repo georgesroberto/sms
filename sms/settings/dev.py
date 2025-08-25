763b3919b3cd3b833ab06de1b80b9f4d983b3b58
georgesroberto <georgesroberto21@gmail.com>
2025-08-25 06:55:17 +0300

    Initialize Django project structure with settings, URLs, and ASGI/WSGI configurations

---
+++ sms/settings/dev.py
@@ -0,0 +1,17 @@
+from .base import *
+
+
+DEBUG = True
+
+
+# Database
+# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    }
+}
+
+STATICFILES_DIRS = [BASE_DIR / "static"]
