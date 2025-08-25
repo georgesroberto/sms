fc3350b7920ebaa5e7b54b27cfefe3fbe98c4fac
georgesroberto <georgesroberto21@gmail.com>
2025-08-25 07:44:26 +0300

    Configure development settings with django-browser-reload and static/media file handling

--- sms/settings/dev.py
+++ sms/settings/dev.py
@@ -1,17 +1,26 @@
 from .base import *
-
+import os
 
 DEBUG = True
+ALLOWED_HOSTS = ["*"]
 
+INSTALLED_APPS += [
+    "django_browser_reload",
+]
 
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+MIDDLEWARE += [
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
+]
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# Static/Media
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 STATICFILES_DIRS = [BASE_DIR / "static"]
